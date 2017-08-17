4953118693eb0613dc8a449a000df9866796a16a
Dan McCandless <dkmccandless@gmail.com>
2017-08-16 19:53:33 -0700

    implement tapered evaluation

--- eval.go
+++ eval.go
@@ -1,6 +1,28 @@
 package main
 
-var pieceEval = [6]int{0, 100, 320, 333, 500, 900}
+const (
+	opening = iota
+	endgame
+
+	pawnPhase   = 1
+	knightPhase = 2
+	bishopPhase = 2
+	rookPhase   = 4
+	queenPhase  = 8
+
+	totalPhase = 2 * (queenPhase + 2*rookPhase + 2*bishopPhase + 2*knightPhase + 8*pawnPhase)
+)
+
+// The static evaluation of each type of Piece.
+var pieceEval = [6][2]int{
+	// opening, endgame
+	{},
+	{100, 100},
+	{320, 300},
+	{315, 335},
+	{500, 525},
+	{900, 900},
+}
 
 // Piece-square tables modifying the evaluation of a Piece depending on its Square.
 type pieceSquare [64]int
@@ -20,6 +42,7 @@ var ps = [2][6]pieceSquare{
 	{},
 	{
 		{},
+		// Black pawn
 		{
 			0, 0, 0, 0, 0, 0, 0, 0,
 			50, 50, 50, 50, 50, 50, 50, 50,
@@ -30,6 +53,7 @@ var ps = [2][6]pieceSquare{
 			5, 10, 10, -20, -20, 10, 10, 5,
 			0, 0, 0, 0, 0, 0, 0, 0,
 		},
+		// Black knight
 		{
 			-50, -40, -30, -30, -30, -30, -40, -50,
 			-40, -20, 0, 5, 5, 0, -20, -40,
@@ -40,6 +64,7 @@ var ps = [2][6]pieceSquare{
 			-40, -20, 0, 5, 5, 0, -20, -40,
 			-50, -40, -30, -30, -30, -30, -40, -50,
 		},
+		// Black bishop
 		{
 			-20, -10, -10, -10, -10, -10, -10, -20,
 			-10, 0, 0, 0, 0, 0, 0, -10,
@@ -50,6 +75,7 @@ var ps = [2][6]pieceSquare{
 			-10, 5, 0, 0, 0, 0, 5, -10,
 			-20, -10, -10, -10, -10, -10, -10, -20,
 		},
+		// Black rook
 		{
 			0, 0, 0, 0, 0, 0, 0, 0,
 			5, 10, 10, 10, 10, 10, 10, 5,
@@ -60,6 +86,7 @@ var ps = [2][6]pieceSquare{
 			-5, 0, 0, 0, 0, 0, 0, -5,
 			0, 0, 0, 5, 5, 0, 0, 0,
 		},
+		// Black queen
 		{
 			-20, -10, -10, -5, -5, -10, -10, -20,
 			-10, 0, 0, 0, 0, 0, 0, -10,
@@ -73,29 +100,75 @@ var ps = [2][6]pieceSquare{
 	},
 }
 
+var kingps = [2][2]pieceSquare{
+	{},
+	{
+		// Black opening
+		{
+			-30, -40, -40, -50, -50, -40, -40, -30,
+			-30, -40, -40, -50, -50, -40, -40, -30,
+			-30, -40, -40, -50, -50, -40, -40, -30,
+			-30, -40, -40, -50, -50, -40, -40, -30,
+			-20, -30, -30, -40, -40, -30, -30, -20,
+			-10, -20, -20, -20, -20, -20, -20, -10,
+			20, 20, 0, 0, 0, 0, 20, 20,
+			20, 30, 10, 0, 0, 10, 30, 20,
+		},
+		// Black endgame
+		{
+			-50, -40, -30, -20, -20, -30, -40, -50,
+			-30, -20, -10, 0, 0, -10, -20, -30,
+			-30, -10, 20, 30, 30, 20, -10, -30,
+			-30, -10, 30, 40, 40, 30, -10, -30,
+			-30, -10, 30, 40, 40, 30, -10, -30,
+			-30, -10, 20, 30, 30, 20, -10, -30,
+			-30, -30, 0, 0, 0, 0, -30, -30,
+			-50, -30, -30, -30, -30, -30, -30, -50,
+		},
+	},
+}
+
 func init() {
-	for piece := range ps[White] {
+	for piece := range ps[Black] {
 		ps[White][piece] = ps[Black][piece].flip()
+	}
+	for phase := range kingps[Black] {
+		kingps[White][phase] = kingps[Black][phase].flip()
 	}
 }
 
 // Eval returns a Position's evaluation score in centipawns relative to White.
 func Eval(pos Position) int {
-	var whiteMaterial, blackMaterial int
+	wp := PopCount(pos.b[White][Pawn])
+	wn := PopCount(pos.b[White][Knight])
+	wb := PopCount(pos.b[White][Bishop])
+	wr := PopCount(pos.b[White][Rook])
+	wq := PopCount(pos.b[White][Queen])
+
+	bp := PopCount(pos.b[Black][Pawn])
+	bn := PopCount(pos.b[Black][Knight])
+	bb := PopCount(pos.b[Black][Bishop])
+	br := PopCount(pos.b[Black][Rook])
+	bq := PopCount(pos.b[Black][Queen])
+
+	phase := queenPhase*(wq+bq) + rookPhase*(wr+br) + bishopPhase*(wb+bb) + knightPhase*(wn+bn) + pawnPhase*(wp+bp)
+
+	var eval int
 	for sq := a1; sq <= h8; sq++ {
-		c, p, ok := pos.PieceOn(sq)
-		switch {
+		switch c, p, ok := pos.PieceOn(sq); {
 		case !ok:
 			continue
 		case p == King:
-			continue
-		case c == White:
-			whiteMaterial += pieceEval[p] + ps[White][p][sq]
-		case c == Black:
-			blackMaterial += pieceEval[p] + ps[Black][p][sq]
+			eval += evalMult(c) * taper(kingps[c][opening][sq], kingps[c][endgame][sq], phase)
+		default:
+			eval += evalMult(c) * (taper(pieceEval[p][opening], pieceEval[p][endgame], phase) + ps[c][p][sq])
 		}
 	}
-	return whiteMaterial - blackMaterial
+	return eval
+}
+
+func taper(open, end, phase int) int {
+	return (open*phase + end*(totalPhase-phase)) / totalPhase
 }
 
 // evalMult returns an evaluation sign multiplier of 1 for White and -1 for Black.
